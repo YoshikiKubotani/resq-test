66768ad9c26ea6046de5d4f0e448a9f5a6577131
botany <yoshikikubotani.lab@gmail.com>
2025-03-04 13:21:23 +0900

    :sparkles: Update to use `pydantic-settings` for config

---
+++ applications/backend/src/settings.py
@@ -0,0 +1,53 @@
+
+import json
+from typing import List
+from pydantic_settings import BaseSettings, field_validator
+
+class Settings(BaseSettings, case_sensitive=True):
+    """The settings for the application.
+
+    Attributes:
+        PROJECT_NAME (str): The name of the project.
+        OPENAI_API_KEY (str): The API key for the OpenAI API.
+        CORS_ALLOW_ORIGINS (List[str]): The origins that are allowed to make requests to the API.
+    """
+    PROJECT_NAME: str = "ResQ API"
+
+    # OpenAI API key
+    OPENAI_API_KEY: str = ""
+
+    # CORS_ALLOW_ORIGINS is a JSON-formatted list of origins that are allowed to make requests to the API.
+    # e.g: '["http://localhost", "http://localhost:8080"]'
+    CORS_ALLOW_ORIGINS: List[str]
+    @field_validator("CORS_ALLOW_ORIGINS", mode="before")
+    @classmethod
+    def validate_cors_origins(cls, v: str) -> str:
+        """Validate the CORS_ALLOW_ORIGINS field.
+
+        Args:
+            v (str): The value of the CORS_ALLOW_ORIGINS field.
+
+        Raises:
+            ValueError: If CORS_ALLOW_ORIGINS is invalid.
+
+        Returns:
+            list[str]: The validated value of the CORS_ALLOW_ORIGINS field.
+        """
+        try:
+            parsed_data = json.loads(v)
+        except json.JSONDecodeError as e:
+            raise ValueError(
+                "JSON decode failed. CORS_ALLOW_ORIGINS must be a JSON-formatted list."
+            ) from e
+
+        if isinstance(parsed_data, list) and all(
+            isinstance(elem, str) for elem in parsed_data
+        ):
+            return v
+        else:
+            raise ValueError(
+                "The decoded JSON was not a list of strings. CORS_ALLOW_ORIGINS must be a JSON-formatted list."
+            )
+
+
+settings = Settings()
