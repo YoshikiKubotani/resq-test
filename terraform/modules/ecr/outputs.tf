e3ede7484b9bdeedc689aff46028e38588ee72c6
botany <yoshikikubotani.lab@gmail.com>
2025-03-16 12:08:11 +0900

    :art: Remove unused outputs

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -2,8 +2,3 @@ output "repository_url" {
   description = "ECR Repository URL"
   value       = aws_ecr_repository.main.repository_url
 }
-
-output "repository_arn" {
-  description = "ECR Repository ARN"
-  value       = aws_ecr_repository.main.arn
-}
