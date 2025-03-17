eae369d84643e59eac22bf8ef1e3705a66384e82
botany <yoshikikubotani.lab@gmail.com>
2025-03-17 23:15:40 +0900

    :bug: Add missing ouput

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -2,3 +2,8 @@ output "repository_url" {
   description = "ECR Repository URL"
   value       = aws_ecr_repository.main.repository_url
 }
+
+output "repository_arn" {
+  description = "ECR Repository ARN"
+  value       = aws_ecr_repository.main.arn
+}
