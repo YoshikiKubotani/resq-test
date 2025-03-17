cc3d65d70f348e35cda736f628006730ea06591c
botany <yoshikikubotani.lab@gmail.com>
2025-03-17 10:57:45 +0900

    :art: Remove commented part

--- terraform/application/main.tf
+++ terraform/application/main.tf
@@ -25,26 +25,6 @@ module "lambda" {
   allowed_origins    = var.allowed_origins
 }
 
-# module "lambda" {
-#   source = "terraform-aws-modules/lambda/aws"
-#   version = "7.20.1"
-
-#   function_name = "${var.project_name}-${var.environment}"
-
-#   create_package = false
-
-#   image_uri    = "${var.ecr_repository_url}:${var.image_tag}"
-#   package_type = "Image"
-
-#   memory_size = var.lambda_memory_size
-#   timeout     = var.lambda_timeout
-
-#   tags = {
-#     Environment = var.environment
-#     Project     = var.project_name
-#     ManagedBy   = "terraform"
-#   }
-# }
 
 # Output definitions
 output "function_url" {
